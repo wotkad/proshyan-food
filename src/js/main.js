36be56a28b0b3a01bd851ed036e7d5743547035a
wotkad <wotkad@gmail.com>
2018-09-25 13:10:52 +0300

    fixes, add style fot reload page

--- src/js/main.js
+++ src/js/main.js
@@ -230,3 +230,16 @@ window.onkeydown = function( event ) {
 		body.style.overflow = '';
 	}
 };
+
+$(document).ready(function(){
+  var body = $("body");
+  body.fadeIn(400);
+  $(document).on("click", "a:not([href^='#']):not([href^='tel']):not([href^='mailto'])", function(e) {
+  e.preventDefault();
+  $("body").fadeOut(400);
+  var self = this;
+  setTimeout(function () {
+    window.location.href = $(self).attr("href");
+  }, 400);
+  });
+});
